b101d1b9dc9d99474b01787b4bb0fccb0d9372c4
Hugo Pereira <134457242+HugoPereiraP@users.noreply.github.com>
2026-06-22 15:35:08 -0300

    Recreate roles and permissions in DCL.sql

--- scripts/ DCL.sql
+++ scripts/ DCL.sql
@@ -1,7 +1,7 @@
 CREATE ROLE role_zenix_admin;
 CREATE ROLE role_zenix_app;
 
--- O administrador possui controle total
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON Usuario TO role_zenix_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON Categoria TO role_zenix_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON Jogo TO role_zenix_admin;
@@ -13,7 +13,7 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON BibliotecaUsuario TO role_zenix_admin;
 GRANT EXECUTE ON prc_realizarCompra TO role_zenix_admin;
 GRANT EXECUTE ON prc_cancelarCompra TO role_zenix_admin;
 
--- Restringe a aplicação de realizar
+
 GRANT SELECT ON Jogo TO role_zenix_app;
 GRANT SELECT ON Categoria TO role_zenix_app;
 GRANT SELECT, UPDATE(saldo) ON Usuario TO role_zenix_app; 
