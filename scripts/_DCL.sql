ffc434cb20baf04de85491248ce659164d01df8e
Mr.Arthur <138824681+MrArthur15@users.noreply.github.com>
2026-06-23 20:04:58 -0300

    ...

---
+++ scripts/_DCL.sql
@@ -0,0 +1,26 @@
+CREATE ROLE role_zenix_admin;
+CREATE ROLE role_zenix_app;
+
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON Usuario TO role_zenix_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Categoria TO role_zenix_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Jogo TO role_zenix_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Cupom TO role_zenix_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Pedido TO role_zenix_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ItemPedido TO role_zenix_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON BibliotecaUsuario TO role_zenix_admin;
+
+GRANT EXECUTE ON prc_realizarCompra TO role_zenix_admin;
+GRANT EXECUTE ON prc_cancelarCompra TO role_zenix_admin;
+
+
+GRANT SELECT ON Jogo TO role_zenix_app;
+GRANT SELECT ON Categoria TO role_zenix_app;
+GRANT SELECT, UPDATE(saldo) ON Usuario TO role_zenix_app; 
+GRANT SELECT ON Cupom TO role_zenix_app;
+GRANT SELECT ON BibliotecaUsuario TO role_zenix_app;
+
+GRANT EXECUTE ON prc_realizarCompra TO role_zenix_app;
+GRANT EXECUTE ON prc_cancelarCompra TO role_zenix_app;
+
+COMMIT;
